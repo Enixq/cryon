f5db182c4954bc836ecdc040a9799ed5c95eb4d7
Alexander <alex@cryon.local>
2026-09-13 09:58:29 +0700

    release: prepare v0.2.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ru.cryon.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.2.2"
+        versionCode = 4
+        versionName = "0.2.3"
     }
 
     buildTypes {
